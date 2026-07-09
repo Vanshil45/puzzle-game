716a7f8a8525bf813e9f743f1beca264369fb848
Vanshil45 <136326183+Vanshil45@users.noreply.github.com>
2026-07-09 19:43:31 +0530

    Integrate Calculator component into the homepage

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Puzzle Game</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default Home;
